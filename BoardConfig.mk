c41754ca35cb0294ecaa72586d038b92b4daabce
Ansh <singhansh64321@gmail.com>
2024-11-19 06:54:32 +0000

    RMX2001L1: Rising from the bottom!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,13 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 include device/realme/RM6785-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := RMX2001,RMX2151,RMX2001L1,RMX2003L1,RMX2005L1,RMX2002L1,RMX2151L1,RM6785
 
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
--- lineage_RMX2001L1.mk
+++ lineage_RMX2001L1.mk
@@ -1,11 +1,20 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 $(call inherit-product, device/realme/RMX2001L1/full_RMX2001L1.mk)
 
-# Inherit common LineageOS stuff.
+# Inherit common RisingOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_RMX2001L1
+
+# RisingOS flags
+PRODUCT_NO_CAMERA := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_PIXEL_FEATURES := true
+TARGET_ENABLE_BLUR := true
+RISING_MAINTAINER := αиѕн
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="αиѕн"
